d75fb11c8bd95832243d598ac126a1f669a193ac
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2024-08-08 18:04:47 -0400

    Upload JLetter.java

---
+++ Module 1/Project 1/JLetter.java
@@ -0,0 +1,28 @@
+/**
+ * A simple program displaying a block letter graphic.
+ *
+ * Module 1 Project 1
+ * @author Addie Domanico - CPSC 1210 - AO1
+ * @version 1/19/2023
+ */
+ 
+public class JLetter {
+   /**
+    * Prints a block letter displaying the letter J created from multiple
+    * alphabetic characters.
+    * @param args Command line arguments - not used.
+    */
+    
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
