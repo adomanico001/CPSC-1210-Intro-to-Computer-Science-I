7c3045f49986290b3e99f200b8c489cfd9af84e9
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2024-08-08 18:00:48 -0400

    Upload Activity 1

---
+++ StudentInfo.java
@@ -0,0 +1,21 @@
+/**
+ * A simple program of student information.
+ *
+ * Module 1 Activity 1
+ * @author Addie Domanico - CPSC 1210 - AO1
+ * @version 1/18/2023
+ */
+
+public class StudentInfo {
+/**
+ * Prints name and previous computer courses.
+ * @param args Command line arguments - not used.
+ */
+   public static void main(String[] args) {
+      System.out.println("Name: Addie Domanico");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   Foundations in Application Development");
+      System.out.println("   Introduction to Scripting");
+      System.out.println("   Programming Languages");
+   }
+}
