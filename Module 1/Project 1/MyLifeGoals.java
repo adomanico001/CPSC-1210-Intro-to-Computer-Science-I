45bedb9c6055c4f4ab9cbfa6f93271f27dbf575b
Adelina Domanico <102832335+adomanico001@users.noreply.github.com>
2024-08-08 18:05:21 -0400

    Upload MyLifeGoals.java

---
+++ Module 1/Project 1/MyLifeGoals.java
@@ -0,0 +1,28 @@
+/**
+ * A simple program listing personal life goals.
+ *
+ * Module 1 Project 1
+ * @author Addie Domanico - CPSC 1210 - AO1
+ * @version 1/19/2023
+ */
+ 
+public class MyLifeGoals {
+  /**
+   * Prints short, medium, and long-term life goals.
+   * @param args Command line arguments - not used.
+   */
+   
+   public static void main(String[] args) {
+      System.out.println("Addie Domanico");
+      System.out.println();
+      System.out.println("My short-term life goals include working on Spring "
+         + "2023 semester courses, with the aim to achieve A's in all four "
+         + "classes. Another goal includes finding co-ops to apply for.");
+      System.out.println("My medium-term life goals include graduating from "
+         + "Auburn with a computer science degree. I then hope to find a job "
+         + "in Atlanta.");
+      System.out.println("My long-term life goals include working in the data "
+         + "analytics field, paying off my student debt, and buying a quaint "
+         + "little home in Atlanta, GA.");
+   }
+}
